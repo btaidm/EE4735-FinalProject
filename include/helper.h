593bed259c48a1ef22dbc5be00903edf195be497
Tim Bradt <tjbradt@mtu.edu>
2015-04-28 22:43:03 -0400

    Added some comments to helper.h

--- include/helper.h
+++ include/helper.h
@@ -5,8 +5,11 @@
 
 #define STRINGIFY(a) #a
 
+// Defines the type of port for the GPIO registers
 #define PORT_TYPE volatile unsigned char
 
+
+// Defines a macro for Interrupts the Works with GCC and IAR
 #if defined(__ICC430__) // Check for IAR vs GCC
 #define __INTERRUPT(x) \
     _Pragma( STRINGIFY( vector=  x ) ) \
@@ -18,49 +21,6 @@
 #endif
 
 #define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
-
-#if 0
-#define TO_HEX(i) ((i) <= 9? '0' + (i) : 'A' - 10 + (i))
-#else
-#define TO_HEX(i) (i)
-#endif
-
-
-inline uint16_t uint16ToChar(uint16_t num, char* temp)
-{
-    temp[0] = TO_HEX((num & 0xF000) >> 12);
-    temp[1] = TO_HEX((num & 0x0F00) >> 8);
-    temp[2] = TO_HEX((num & 0x00F0) >> 4);
-    temp[3] = TO_HEX((num & 0x000F) >> 0);
-    temp[4] = 0;
-
-    return 4;
-
-}
-
-inline uint32_t uint32ToChar(uint32_t num, char* temp)
-{
-    temp[0] = TO_HEX((num & 0xF0000000) >> 28);
-    temp[1] = TO_HEX((num & 0x0F000000) >> 24);
-    temp[2] = TO_HEX((num & 0x00F00000) >> 20);
-    temp[3] = TO_HEX((num & 0x000F0000) >> 16);
-    temp[4] = TO_HEX((num & 0x0000F000) >> 12);
-    temp[5] = TO_HEX((num & 0x00000F00) >> 8);
-    temp[6] = TO_HEX((num & 0x000000F0) >> 4);
-    temp[7] = TO_HEX((num & 0x0000000F) >> 0);
-    temp[8] = 0;
-
-    return 8;
-}
-
-inline uint8_t uint8ToChar(uint8_t num, char* temp)
-{
-    temp[0] = TO_HEX((num & 0xF0) >> 4);
-    temp[1] = TO_HEX((num & 0x0F) >> 0);
-    temp[2] = 0;
-
-    return 2;
-}
 
 static inline int8_t sign32(int32_t x)
 {
